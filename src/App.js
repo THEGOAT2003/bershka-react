e0c959f4e9961b7ab04381435dad1aeea7765a48
i-am-iron-man <fabianvanderstoel12@gmail.com>
2024-02-12 00:06:12 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Data from './components/bershka';
+
+function App() {
+  return (
+    <div className="App">
+      <Data></Data>
+    </div>
+  );
+}
+
+export default App;
